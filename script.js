5631ed2111ce4f72cd8afcfed36ed5651df9bff9
ogadra <*****@users.noreply.github.com>
2020-09-30 13:23:41 +0900

    cut algorithm improved

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
 // var kuromoji = require("kuromoji");
 
+function breakCheck(word, prepos){
+    // 続ける場合はFalse, 分ける単語が来た場合はTrueを返す
+      if (word.pos==="助詞" || word.pos==="助動詞") {
+          return false
+      } else if (prepos.pos_detail_1 ==="括弧閉") {
+          return true
+      } else if (prepos.pos_detail_1 === "括弧開") {
+          return false
+      }  else if (prepos.pos === "名詞" && word.pos === "名詞") {
+          return false
+      } else if (word.pos_detail_1=="サ変接続") {
+        return true
+      } else if (word.pos_detail_1 === "非自立") {
+          return false
+      } else if (prepos.pos === "接頭詞") {
+          console.log(prepos);
+          return false
+      } else if (word.pos_detail_1 === "接尾"){
+          return false
+      } else if (word.conjugated_type === "サ変・スル"){
+          return false
+      } else if (word.pos ==="記号") {
+          if (word.pos_detail_1==="括弧開"){
+            return true
+          } else {
+            return false
+          }
+      } else {
+          return true
+      }
+    
+    }
 
 document.getElementById("btn").onclick = function(){
     var phrase = document.getElementById("phrase").value;
@@ -8,16 +40,14 @@ document.getElementById("btn").onclick = function(){
         var path = tokenizer.tokenize(phrase);
         var table = document.getElementById('targetTable');
 		var phrases = [path[0].surface_form];
-		var prepos = path[0].pos;
+        var preword = path[0];
         for (var i = 1; i < path.length; i++){
-			if (path[i].pos==="助詞"||path[i].pos==="助動詞") {
-				phrases[phrases.length - 1] += path[i].surface_form;
-			} else if (prepos === "名詞" && path[i].pos === "名詞") {
-				phrases[phrases.length - 1] += path[i].surface_form;
-			} else {
-				phrases.push(path[i].surface_form);
-            }
-            prepos = path[i].pos;
+                if (breakCheck(path[i], preword)) {
+            phrases.push(path[i].surface_form);
+          }else {
+            phrases[phrases.length - 1] += path[i].surface_form;
+          }
+          preword = path[i];
 
 /*            console.log(path[i].surface_form);
             
